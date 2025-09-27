22052ac27a879539d22ec2c881aa3534ba1f52d7
Paxerr <143940744+Paxerr@users.noreply.github.com>
2025-09-27 18:26:21 +0700

    Update mongodb.js
    theem aggregate //Đưa ra đánh giá theo loại

--- mongodb.js
+++ mongodb.js
@@ -7,7 +7,25 @@
 
 
 
-//agress
+//aggregate
+
+//Đưa ra đánh giá theo loại    
+db.DanhGia.aggregate([
+  {
+    $group: {
+      _id: "$LoaiDanhGia",
+      danhSach: {
+        $push: {
+          MaKhachHang: "$MaKhachHang",
+          MaPhong: "$MaPhong",
+          LoaiPhong: "$LoaiPhong",
+          NDDanhGia: "$NDDanhGia"
+        }
+      }
+    }
+  }
+])
 
 
 //end
+
