d6ba61024cc753fed2230157adea5e1dc8a73caf
tdatd-icy <dinhtiendat2105@gmail.com>
2025-09-28 23:21:18 +0700

    TDatD.js

--- mongodb.js
+++ mongodb.js
@@ -9,6 +9,27 @@
 
 //aggregate
 
+//Tính điểm trung bình đánh giá theo loại phòng
+db.DanhGia.aggregate([
+  {
+    $group: {
+      _id: "$LoaiPhong",
+      DiemTrungBinh: { $avg: "$Diem" }
+    }
+  }
+])
+
+// Thống kê số lượng đánh giá theo mức độ 
+db.DanhGia.aggregate([
+  {
+    $bucket: {
+      groupBy: "$Diem",
+      boundaries: [0, 2, 4, 6],   
+      output: { SoLuong: { $sum: 1 } }
+    }
+  }
+])
+
 //Đưa ra đánh giá theo loại    
 db.DanhGia.aggregate([
   {
@@ -36,6 +57,7 @@ db.LSGiaoDich.aggregate([
 ])
 
 //end
+
 
 
 
